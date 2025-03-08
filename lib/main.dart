e0ff6aa9d4e9a4cdb04a618a5cd63d5d99501eed
Girish Parate <girishparate@Girishs-MacBook-Pro.local>
2025-03-08 06:30:03 +0530

    app-name changes

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Local App',
+      title: 'Shop list',
       darkTheme: ThemeData(brightness: Brightness.dark),
       theme: ThemeData(brightness: Brightness.dark),
       home: MainHomeScreen(),
